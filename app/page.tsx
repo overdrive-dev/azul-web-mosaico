b79e2159b8c0601e2acf3bff33ac7aee318d9c54
Victor Sanches <victor@aieo.com.br>
2026-03-07 13:43:54 -0300

    fix: correct Soluções Offshore images (home: foto-06, page: foto-15)

--- app/page.tsx
+++ app/page.tsx
@@ -224,7 +224,7 @@ export default function Home() {
           </div>
           <div className="order-first lg:order-none lg:w-1/2">
             <div className="relative aspect-square lg:aspect-auto lg:h-[50vw] w-full bg-gray-200">
-              <Image src="/images/foto-01.jpeg" alt="Soluções Offshore" fill className="object-cover" />
+              <Image src="/images/foto-06.JPG" alt="Soluções Offshore" fill className="object-cover" />
             </div>
           </div>
         </div>
